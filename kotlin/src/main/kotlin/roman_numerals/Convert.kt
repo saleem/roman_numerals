d44cef3ba0baba05bb8afa0ecd3a22bd2926d314
Saleem Siddiqui <Siddiqui.Saleem@gmail.com>
2019-09-01 17:41:00 -0500

    Added Kotlin test for descending Roman numerals and for garbage, which is ignored

--- kotlin/src/main/kotlin/roman_numerals/Convert.kt
+++ kotlin/src/main/kotlin/roman_numerals/Convert.kt
@@ -3,6 +3,10 @@ package roman_numerals
 class Convert {
     val ROMAN: HashMap<String, Int> = hashMapOf("I" to 1, "V" to 5, "X" to 10, "L" to 50, "C" to 100, "D" to 500, "M" to 1000)
     fun convert(roman: String): Int? {
-        return ROMAN.get(roman)
+        var value: Int = 0
+        roman.forEach {
+            value += ROMAN.getOrElse(Character.toString(it)) { 0 }
+         }
+         return value
     }
 }
--- kotlin/src/test/kotlin/roman_numerals/ConvertTest.kt
+++ kotlin/src/test/kotlin/roman_numerals/ConvertTest.kt
@@ -4,14 +4,25 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ConvertTest {
+    val convert = Convert()
+
     @Test fun singleRomanLettersShouldConvertToHinduArabicNumbers() {
-        val classUnderTest = Convert()
-        assertEquals(1, classUnderTest.convert("I"), "I should convert to 1")
-        assertEquals(5, classUnderTest.convert("V"), "V should convert to 5")
-        assertEquals(10, classUnderTest.convert("X"), "X should convert to 10")
-        assertEquals(50, classUnderTest.convert("L"), "L should convert to 50")
-        assertEquals(100, classUnderTest.convert("C"), "C should convert to 100")
-        assertEquals(500, classUnderTest.convert("D"), "D should convert to 500")
-        assertEquals(1000, classUnderTest.convert("M"), "M should convert to 1000")
+        assertEquals(1, convert.convert("I"), "I should convert to 1")
+        assertEquals(5, convert.convert("V"), "V should convert to 5")
+        assertEquals(10, convert.convert("X"), "X should convert to 10")
+        assertEquals(50, convert.convert("L"), "L should convert to 50")
+        assertEquals(100, convert.convert("C"), "C should convert to 100")
+        assertEquals(500, convert.convert("D"), "D should convert to 500")
+        assertEquals(1000, convert.convert("M"), "M should convert to 1000")
+    }
+
+    @Test fun ascendingRomanLettersShouldBeAdded() {
+        assertEquals(6, convert.convert("VI"), "VI should convert to 6")
+        assertEquals(1666, convert.convert("MDCLXVI"), "MDCLXVI should convert to 1666")
+        assertEquals(2020, convert.convert("MMXX"), "MMXX should convert to 2020")
+    }
+
+    @Test fun shouldIgnoreUnrecognizableTextWithinValidRomanNumerals() {
+        assertEquals(6, convert.convert("Very😀Important!بہت اہم"), "Very😀Important!بہت اہم should convert to 6")
     }
 }
